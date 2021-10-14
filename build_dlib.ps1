ee62599bb48d1eda61bab9b2e3fd9abceda0ad51
yuzuhakuon <yuzuhakuon@gmail.com>
2021-10-14 10:03:11 +0800

    build: build dlib

---
+++ build_dlib.ps1
@@ -0,0 +1,30 @@
+function BuildForWindows($platform, $build_type) {
+    $build_dir = "build"
+    mkdir $build_dir -Force -ErrorAction Stop | Out-Null
+    cd $build_dir
+    pwd
+
+    if ($platform -eq "x64") {
+        $msbuild_platform = "x64"
+    }
+    else {
+        $msbuild_platform = "Win32"
+    }
+
+
+
+    cmake -G "Visual Studio 16 2019" `
+        -A $msbuild_platform `
+        -D CMAKE_BUILD_TYPE=${build_type} `
+        -D CMAKE_INSTALL_PREFIX=install `
+        -D DLIB_NO_GUI_SUPPORT=OFF `
+        -D DLIB_USE_BLAS=ON `
+        -D DLIB_GIF_SUPPORT=ON `
+        -D DLIB_PNG_SUPPORT=ON `
+        -D DLIB_JPEG_SUPPORT=ON `
+        -D DLIB_USE_CUDA=OFF `
+
+    msbuild INSTALL.vcxproj /t:build /p:configuration=$build_type /p:platform=$msbuild_platform -maxcpucount
+    ls
+    cd ..
+}
